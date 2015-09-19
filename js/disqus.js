ed0f55c1740adc5936326f803fb38b643d951131
atmorojo <satmo54@gmail.com>
2015-09-19 08:59:28 +0700

    fix comments

--- js/disqus.js
+++ js/disqus.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('.show-comments').on('click', function(){
         var disqus_shortname = 'atmorojo'; // Replace this value with *your* username.
 
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = false;
         dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
         $(this).fadeOut();
